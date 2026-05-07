4b029bb52973ce01532d2ab647ded7f48cfccc11
Abdulrehman <abdulrehman003cui@gmail.com>
2026-05-07 21:53:44 +0500

    hello

--- Project/src/ques1.java
+++ Project/src/ques1.java
@@ -1,3 +1,12 @@
 abstract class Animali{
     abstract void sound();
 }
+
+class Human extends Animali{
+    void sound(){
+        System.out.println("");
+    }
+}
+class Abdulrehman extends Human{
+
+}
